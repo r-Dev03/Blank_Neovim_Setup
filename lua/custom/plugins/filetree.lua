7e9fcb772e6f4449a8c637f6789d213bcde3303d
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-05-26 14:56:01 -0500

    Added new keymap for focus on neotree

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -8,7 +8,8 @@ return {
 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
 	},
-	config = function ()
+	config = function()
 		require('neo-tree').setup {}
+		vim.keymap.set('n', '<C-f>', '<Cmd>Neotree focus<CR>')
 	end,
 }
