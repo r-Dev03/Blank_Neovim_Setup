957489a207635918375fb4ec85732a1c709d911f
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-06-02 16:22:50 -0500

    Added remap for ctrl backspace to delete an entire word, removed tressitter file for now.

--- init.lua
+++ init.lua
@@ -43,6 +43,7 @@ vim.g.maplocalleader = ' '
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("i", "<C-BS>", "<Esc>cvb", {})
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
