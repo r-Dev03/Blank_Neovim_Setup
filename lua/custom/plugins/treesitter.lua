0159ab475f6493fbb84658171d11605fe76c0020
Ribbal Hussain <ahmedhussain32000@gmail.com>
2023-06-11 17:03:33 -0500

    Installed treesitter.

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -1,11 +1,12 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    { run = ":TSUpdate" },
+    build = ":TSUpdate",
     config = function()
       require 'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the five listed parsers should always be installed)
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "typescript", "c", "html",
+          "rust", "css" },
 
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
